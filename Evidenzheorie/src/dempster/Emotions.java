2486d1526238326e74b1b424e846ab6246162058
Dennis Schmidt <schmiddi11@gmx.de>
2014-01-15 22:34:02 +0100

    JavaDocs bzw Kommentrare Emotions hinzugefuegt

--- Evidenzheorie/src/dempster/Emotions.java
+++ Evidenzheorie/src/dempster/Emotions.java
@@ -1,7 +1,20 @@
 package dempster;
+
+/**
+ * 
+ * @author Dennis Schmidt, Patrick Kalmbach
+ * 
+ * Enumeration die die einzelnen Emotionen speichert.
+ *
+ */
 public enum Emotions {
 	VERACHTUNG, EKEL, WUT, UEBERRASCHUNG, ANGST, FREUDE;
 	
+	/**
+	 * Wandelt die Enumerationswerte in einen gleichnamigen
+	 * String um, bei dem nur der erste Buchstabe gross
+	 * geschrieben ist.
+	 */
 	public String toString()
 	{
 	    switch(this){
@@ -14,6 +27,13 @@ public enum Emotions {
 	    	default:    		return "";
 	    }
 	}
+	
+	/**
+	 * Die Methode dient nur der Rueckgabe einer Liste die
+	 * alle Emotionen enthaelt.
+	 * 
+	 * @return Eine Liste die alle Emotionen enthaelt.
+	 */
 	public static Emotions[] all(){
 		return new Emotions[]{VERACHTUNG,EKEL,WUT,UEBERRASCHUNG,ANGST,FREUDE};
 	}
